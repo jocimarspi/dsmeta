c581fd6d6f15de01480c150d438ca7ae882b00ad
Jocimar B. A. Huss <jocimar.huss@db1.com.br>
2022-07-11 21:28:52 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>Oi Galera!</h1>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
       <NotificationButton />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
